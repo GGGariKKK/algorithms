caaab3800842f842000601bed0bb3df31eb8d30a
Illia Harkusha <harkusha.kp13@gmail.com>
2022-09-09 14:40:42 +0300

    Initial commit

---
+++ src/main/java/algorithms/AlgoTest.java
@@ -0,0 +1,14 @@
+package algorithms;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+public class AlgoTest {
+    public static void main(String[] args) {
+        Collection<Integer> c = new LinkedList<Integer>(List.of(1, 2, 3, 4, 5));
+
+    }
+
+
+}
---
+++ src/main/java/algorithms/Person.java
@@ -0,0 +1,16 @@
+package algorithms;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private int height;
+    private int weight;
+    private int age;
+
+    public Person(int height, int weight, int age) {
+        this.height = height;
+        this.weight = weight;
+        this.age = age;
+    }
+}
